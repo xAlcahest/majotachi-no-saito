4082c92a462be0542c5b690a17474537466e7e7e
Ericchi <ericchiyukia@gmail.com>
2022-04-04 21:51:49 +0200

    Modali

--- public/js/bufferaepisodi.js
+++ public/js/bufferaepisodi.js
@@ -1,3 +1,11 @@
+function modali(){
+    $(document).ready(function(){
+      $('.modal').modal({
+        dismissible: false,
+        complete: function() { alert('Closed triggered'); }
+        });
+      });
+    }
 
 window.onload = modali;
 
--- public/js/modali.js
+++
@@ -1,9 +0,0 @@
-function modali(){
-    $(document).ready(function(){
-      $('.modal').modal({
-        dismissible: false,
-        complete: function() { alert('Closed triggered'); }
-        });
-    console.log("Modali caricati.")  
-    });
-}
