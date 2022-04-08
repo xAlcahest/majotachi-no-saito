86e8ed243fa1d73789fb334fa2dea5fa3c87f114
Ericchi <ericchiyukia@gmail.com>
2022-04-08 23:55:18 +0200

    Ottimizzato il js

---
+++ public/js/decorastreghe.js
@@ -0,0 +1,11 @@
+function beatoriche() {
+    var laugh = document.getElementById("ahaha");
+    laugh.play();
+    document.getElementById("beatoriche").src = "images/Bea_a11_akuwarai5.png";
+}
+
+$(function(){ 
+    $("#bar1").load("bar1.html");
+    $("#bar2").load("bar2.html");
+    $("#footer").load("footer.html");  
+}); 
