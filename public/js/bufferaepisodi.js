bfe29d3d2f52ebdb3a125d57f19c196c410f3fd7
Ericchi <git@ericchi.moe>
2022-10-13 17:30:57 +0200

    Tema blu e tema rosso

--- public/js/bufferaepisodi.js
+++ public/js/bufferaepisodi.js
@@ -19,11 +19,11 @@ function bufferaepisodi(softavailable, telegramsoft, titoliavailable, episodi, t
         if (titoliavailable === 1) {
             buffer += '<td scope="row">' + titoli[i] + '</td>';
         }
-        buffer += '<td scope="row" class="right-align">' + '<a class="waves-effect waves-light btn modal-trigger purple" href="#ep' + (i + 1) + '"><i class="material-icons">play_arrow</i></a><a class="waves-effect waves-light btn modal-trigger purple" href="' + episodi[i] + '?dl"><i class="material-icons">file_download</i></a><div id="ep' + (i + 1) + '" class="modal"><div class="modal-content no-pad"><div id="playerbox' + (i + 1) + '" class="bganime"></div></div><div class="modal-footer"><a href="' + episodi[i] + '" class="waves-effect waves-purple btn-flat" download><i class="large material-icons left">file_download</i>Hardsub</a><a class="modal-action modal-close waves-effect waves-purple btn-flat" onclick="terminaanime()"><i class="large material-icons left">close</i>Chiudi</a></div></div></td></tr>';
+        buffer += '<td scope="row" class="right-align">' + '<a class="waves-effect waves-light btn modal-trigger blue darken-4" href="#ep' + (i + 1) + '"><i class="material-icons">play_arrow</i></a><a class="waves-effect waves-light btn modal-trigger blue darken-4" href="' + episodi[i] + '?dl"><i class="material-icons">file_download</i></a><div id="ep' + (i + 1) + '" class="modal"><div class="modal-content no-pad"><div id="playerbox' + (i + 1) + '" class="bganime"></div></div><div class="modal-footer"><a href="' + episodi[i] + '" class="waves-effect waves-blue btn-flat" download><i class="large material-icons left">file_download</i>Hardsub</a><a class="modal-action modal-close waves-effect waves-blue btn-flat" onclick="terminaanime()"><i class="large material-icons left">close</i>Chiudi</a></div></div></td></tr>';
     }
     buffer += '</tbody></table>';
     if (telegramsoft === 1) {
-        buffer += '<p class="light center">Gli episodi softsub di questo anime sono disponibili su Telegram!</p><p class="center"><a class="waves-effect waves-light btn modal-trigger purple" href="' + telegramlink + '" target="_blank">Vai al canale Telegram</a></p>'
+        buffer += '<p class="light center">Gli episodi softsub di questo anime sono disponibili su Telegram!</p><p class="center"><a class="waves-effect waves-light btn modal-trigger blue darken-4" href="' + telegramlink + '" target="_blank">Vai al canale Telegram</a></p>'
     }
 
     console.log("Episodi caricati.");
